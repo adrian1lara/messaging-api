ec3cb322a69a11b7946c39127590481fad526ac8
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-06 22:58:47 -0500

    add model interface and schema for chat collection

---
+++ src/models/chat.ts
@@ -0,0 +1,17 @@
+import { Schema, Types, model } from "mongoose";
+
+interface IChat {
+    type: String,
+    participants: Types.ObjectId[],
+    created_at: Date
+}
+
+const chatSchema = new Schema<IChat>({
+    type: { type: String, default: 'one_to_one' },
+    participants: { type: [Schema.Types.ObjectId], ref: 'User' },
+    created_at: { type: Date, default: Date.now }
+})
+
+const Chat = model<IChat>('Chat', chatSchema)
+
+export default Chat

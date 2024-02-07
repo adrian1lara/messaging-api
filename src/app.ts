f5dbdd1c6e3a70b87d73e95fa28a88b7d2aa4bae
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-06 20:24:59 -0500

    add error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, Express} from 'express'
+import express, {Request, Response, NextFunction} from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
@@ -11,7 +11,23 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!")
 })
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+export interface CustomError extends Error { 
+    statusCode?: number;
+  }
+  
+
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+    const error: CustomError = new Error('Not found');
+    error.statusCode = 404;
+    next(error);
+  }); 
+  app.use((err: CustomError, _: Request, res: Response) => { 
+    const statusCode = err.statusCode || 500; 
+    const name = err.name || 'Error'; 
+    res 
+      .status(statusCode) 
+      .json({ name, message: err.message }); 
+  }); 
+
+
+export default app

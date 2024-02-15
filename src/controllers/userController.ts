5def550854ab2c8419ef15e1cb5c26be5076a46a
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-15 00:57:23 -0500

    handle encrypt password

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express"
 import User  from "../models/user"
+import { hashSync } from "bcrypt"
 
 export const createUser = async (req: Request, res: Response) => {
     try {
         const { email, username, password } = req.body
         if(!email || !username || !password) return res.status(400).send("All fields are required")
 
-        const user = new User({ email, username, password })
+        const hashedpass = hashSync(req.body.password, 10)
+
+        const user = new User ({email, username, password: hashedpass})
         await user.save()
         res.send(user)
+
 
     } catch (error) {
         console.error(error)

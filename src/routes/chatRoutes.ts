83915cca8ab21199a53e1462c5316770f4fff724
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-15 22:16:54 -0500

    add get route to chat

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createChat } from "../controllers/chatController";
+import { createChat, getChats } from "../controllers/chatController";
 
 const chatRouter = Router()
 
 chatRouter.post("/newChat", createChat)
+
+chatRouter.get("/chats", getChats)
 
 export default chatRouter

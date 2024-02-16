dff628c10a33add5bef4cb10aea80d636a623c31
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-15 19:26:35 -0500

    handle route tu get users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import User from "../models/user";
-import { createUser } from "../controllers/userController";
+import { createUser, getUsers } from "../controllers/userController";
 
 const userRouter = Router()
 
+
+userRouter.get("/api/users", getUsers)
+
+
 userRouter.post("/api/newUser", createUser)
+
 
 
 export default userRouter

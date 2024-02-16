b26ca37ac0e74d5d880113c55f13fe103c64e2c0
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-15 21:24:48 -0500

    add handle create chat

---
+++ src/controllers/chatController.ts
@@ -0,0 +1,28 @@
+import User from "../models/user"
+import Chat from "../models/chat"
+import { Response , Request} from "express"
+
+export const createChat = async (req: Request, res: Response) => {
+
+    try {
+        
+        const { participants } = req.body
+
+        const existingUsers = await User.find({ _id: { $in : participants }})
+    
+        if(existingUsers.length !== existingUsers.length) {
+            return res.status(400).send("Invalid users ID's provided")
+        }
+    
+        const newChat = new Chat({ participants: participants })
+    
+        await newChat.save()
+    
+        res.status(201).send(newChat)
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send("Something went wrong")
+    }
+   
+}

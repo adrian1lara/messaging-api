05c899866dfa0fd2d9df6233fe34d4c194efad82
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-18 21:57:52 -0500

    add chat objectId in schema and interface

--- src/models/message.ts
+++ src/models/message.ts
@@ -2,6 +2,7 @@ import { Schema, Types, model  } from "mongoose";
 
 interface IMessage {
     content: String,
+    chat: Types.ObjectId,
     sender: Types.ObjectId,
     read_by: Types.ObjectId[],
     timestamp: Date,
@@ -15,6 +16,7 @@ interface IMessage {
 
 const messageSchema = new Schema<IMessage>({
     content: { type: String, required: true },
+    chat: {type: Schema.Types.ObjectId, ref: 'Chat'},
     sender: { type: Schema.Types.ObjectId, ref: 'User' },
     read_by: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     timestamp: { type: Date, default: Date.now },

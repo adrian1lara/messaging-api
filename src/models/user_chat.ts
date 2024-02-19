06036a9161301812289923fdc8b55f10a2935e3e
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-18 22:00:35 -0500

    update single user to array of users

--- src/models/user_chat.ts
+++ src/models/user_chat.ts
@@ -1,13 +1,13 @@
 import { Schema, Types, model  } from "mongoose";
 
 interface User_Chat {
-    user: Types.ObjectId,
+    user: Types.ObjectId[],
     chat: Types.ObjectId,
     joined_at: Date
 }
 
 const user_chatSchema = new Schema<User_Chat>({
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    user: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
     joined_at: { type: Date, default: Date.now }
 })

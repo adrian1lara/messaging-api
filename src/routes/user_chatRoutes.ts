a59b892ddc20d8f3a123d2d873db30c1c73ff536
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-18 22:01:39 -0500

    update name of the roures to clean code

--- src/routes/user_chatRoutes.ts
+++ src/routes/user_chatRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createUser_Chat } from "../controllers/user_chatController";
+import { createUser_Chat, getUser_Chat } from "../controllers/user_chatController";
 
 const user_chatRouter = Router()
 
-user_chatRouter.post("/newUser_Chat", createUser_Chat)
+user_chatRouter.post("/new", createUser_Chat)
+
+user_chatRouter.get("/all", getUser_Chat)
 
 export default user_chatRouter

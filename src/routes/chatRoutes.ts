6d4f012f998721239996d6f08920f3014c2a442c
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-20 00:51:14 -0500

    add route to get chats of one user

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createChat, getChats } from "../controllers/chatController";
+import { createChat, getChats, getUserChats } from "../controllers/chatController";
 
 const chatRouter = Router()
 
 chatRouter.post("/new", createChat)
 
 chatRouter.get("/all", getChats)
+
+chatRouter.get("/all/:userId", getUserChats)
 
 export default chatRouter

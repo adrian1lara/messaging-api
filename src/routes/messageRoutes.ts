2f8194d9ada8d92e52fa2db66e6766eb1716e0bb
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-19 22:06:40 -0500

    add get route to messages in chat

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createMessage } from "../controllers/messageController";
+import { createMessage, getMessagesInChat } from "../controllers/messageController";
 
 const messageRouter = Router()
 
 messageRouter.post("/:userId/:chatId/new", createMessage)
+
+messageRouter.get("/:userId/:chatId/messages", getMessagesInChat)
 
 export default messageRouter

5fc6172164674193a2134c2f374fd04d0be0d266
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-20 00:50:44 -0500

    handle get user chats

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -44,3 +44,28 @@ export const getChats = async(req: Request, res: Response) => {
         
     }
 }
+
+
+export const getUserChats = async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.params
+
+        const user = await User.findById(userId)
+
+        if(!user) {
+            return res.status(404).send("User not found")
+        }
+
+        const userChats = await Chat.find({ participants: userId })
+
+        if(!userChats) {
+            return res.status(404).send("Chats not found")
+        }
+
+        return res.status(201).send(userChats)
+        
+    } catch (error) {
+        console.error(error)
+        return res.status(404).send("Somenthing went wrong :/")
+    }
+}

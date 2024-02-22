d44551444104f000fc053ef96ac7888e0dc3c5ff
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-21 22:35:51 -0500

    add cookieparser and errorHandle of middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, {Request, Response, NextFunction, Application} from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import userRoutes from './routes/userRoutes'
 import chatRouter from './routes/chatRoutes';
 import user_chatRouter from './routes/user_chatRoutes';
 import messageRouter from './routes/messageRoutes';
+import { errorHandler } from './middlewares/errorMiddleware'
 
 
 const app: Application = express();
 app.use(cors())
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use("/api/v0/user", userRoutes)
 app.use("/api/v0/chat", chatRouter)
 app.use("/api/v0/user_chat", user_chatRouter)
@@ -24,6 +27,8 @@ export interface CustomError extends Error {
     statusCode?: number;
   }
   
+
+app.use(errorHandler)
 
 app.use((_req: Request, _res: Response, next: NextFunction) => {
     const error: CustomError = new Error('Not found');

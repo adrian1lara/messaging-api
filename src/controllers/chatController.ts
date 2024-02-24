26b589d8b1c3080d5ac97f0e46ab0f80c2d61e37
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-23 19:21:46 -0500

    add validate of userId parameter

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -50,11 +50,17 @@ export const getUserChats = async (req: Request, res: Response) => {
     try {
         const { userId } = req.params
 
+        if(req.user?._id != userId) {
+            return res.status(403).send("Not authorized, not user account")
+        }
+
         const user = await User.findById(userId)
 
         if(!user) {
             return res.status(404).send("User not found")
         }
+
+
 
         const userChats = await Chat.find({ participants: userId })
 

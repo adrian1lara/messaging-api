2f2884a97808b581f94cf07945ce09e8ffdf31b0
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-27 20:48:24 -0500

    add mw auth to message routes

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createMessage, getMessagesInChat } from "../controllers/messageController";
+import { authenticate } from "../middlewares/auth";
 
 const messageRouter = Router()
 
-messageRouter.post("/:userId/:chatId/new", createMessage)
+messageRouter.post("/:userId/:chatId/new", authenticate, createMessage)
 
-messageRouter.get("/:userId/:chatId/messages", getMessagesInChat)
+messageRouter.get("/:userId/:chatId/messages", authenticate, getMessagesInChat)
 
 export default messageRouter

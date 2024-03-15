13c0e4614fbd9efb14fc990f2a3fab184eb41fc0
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-15 18:58:57 -0500

    update origin of io server and app cors request

--- src/app.ts
+++ src/app.ts
@@ -15,14 +15,14 @@ const app: Application = express();
 export const server = createServer(app)
 export const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     methods:['GET','POST']
   }
 })
 
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["https://chatty-api.fly.dev", "http://localhost:3000"],
   credentials: true,
 })) 
 

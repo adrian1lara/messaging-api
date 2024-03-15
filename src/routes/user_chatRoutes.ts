5a8db475cd7ccf4b5182ad5fbd2aae0b2ae0823e
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-14 22:46:07 -0500

    add comment for now

--- src/routes/user_chatRoutes.ts
+++ src/routes/user_chatRoutes.ts
@@ -3,8 +3,8 @@ import { createUser_Chat, getUser_Chat } from "../controllers/user_chatControlle
 
 const user_chatRouter = Router()
 
-user_chatRouter.post("/new", createUser_Chat)
+//user_chatRouter.post("/new", createUser_Chat)
 
-user_chatRouter.get("/all", getUser_Chat)
+//user_chatRouter.get("/all", getUser_Chat)
 
 export default user_chatRouter

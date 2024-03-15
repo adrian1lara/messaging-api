f95654b8a4908838d72a0f1e94c3f13e75b8d443
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-14 21:58:22 -0500

    add middleware to getchats

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createChat, deleteAllChats, getChatByUserId, getChats, getUserChats } from "../controllers/chatController";
 import { authenticate } from "../middlewares/auth";
+import isAdmin from "../middlewares/admin";
 
 const chatRouter = Router()
 
 chatRouter.post("/new", createChat)
 
-chatRouter.get("/all", getChats)
+chatRouter.get("/all", authenticate, getChats)
 
 chatRouter.get("/:userId/chats", authenticate, getUserChats)
 

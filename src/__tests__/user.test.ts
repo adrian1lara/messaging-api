cf244770954ce3a3bce452640f0c737e69472023
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-16 13:15:18 -0500

    comment last test and add new tests

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,13 +1,17 @@
-import { Request, Response } from "express";
-import User from "../models/user";
-import * as userController from "../controllers/userController"
+import supertest from "supertest";
+import { describe } from "node:test";
+import { app } from "../app";
+import { userInput } from "./fixtures/user.fixture";
+import mongoose from "mongoose";
+import connect from "../DB/connection";
 
+/** 
 describe("createUser", () => {
     it('should add email, username and password to the user', async () => {
             /**
      * Spy on the model save function and return a completed promise.
      * We are not testing the model here, only the controller so this is ok.
-     */
+     
 
         jest.spyOn(User.prototype, 'save').mockImplementationOnce(() => Promise.resolve())
 
@@ -55,3 +59,36 @@ describe("createUser", () => {
         expect(mockResponse.send).toHaveBeenCalledWith("All fields are required")
     });
 })
+*/
+describe('user', () => {
+    beforeEach(async () => {
+        await connect()
+    })
+
+    afterEach(async () => {
+        await mongoose.disconnect();
+        await mongoose.connection.close();
+      });
+
+        describe("given the email, username and password are valid", () => {
+
+            it('should return a 201 status and the user payload', async () => {
+
+                const res = await supertest(app).post("/api/v0/user/new")
+                .send(userInput)
+
+                expect(res.status).toBe(201)
+
+                expect(res.body).toEqual({
+                    "__v": 0,
+                    "_id": expect.any(String),
+                    "role": "user",
+                    "email": "testUser@example.com",
+                    "username": "Test User",
+                    "password": expect.any(String),
+                    "created_at": expect.any(String),
+                })
+            });
+            
+        })
+    })

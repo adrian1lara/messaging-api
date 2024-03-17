cdce317fdc5387b3d47c54098d72cd03909fbe38
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-17 00:34:53 -0500

    add: update avatar and delete users by email routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser, deleteUserAccount, deleteUserByAdmin, findUserByUsername, getUserByToken, getUsers, loginUser } from "../controllers/userController";
+import { createUser, deleteManyUsers, deleteUserAccount, deleteUserByAdmin, findUserByUsername, getUserByToken, getUsers, loginUser, updateAvatar } from "../controllers/userController";
 import { authenticate } from "../middlewares/auth";
 import isAdmin from "../middlewares/admin";
 import limitUsers from "../middlewares/limitUsers";
@@ -17,9 +17,12 @@ userRouter.post("/new", limitUsers, createUser)
 
 userRouter.post("/auth/login", loginUser)
 
+userRouter.put("/update/avatar", authenticate, updateAvatar)
+
 userRouter.delete("/auth/delete/:userId", authenticate, isAdmin, deleteUserByAdmin)
 
 userRouter.delete("/auth/account/delete/:userId", authenticate, deleteUserAccount)
 
+userRouter.delete("/delete/many", authenticate, isAdmin, deleteManyUsers)
 
 export default userRouter
